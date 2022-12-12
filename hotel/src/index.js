11d4fbf8e4b0a4e272345dcbbb26fcff9fd2aa3b
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-12-12 11:37:43 -0300

    Update index.js

--- hotel/src/index.js
+++ hotel/src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Willyan</h1>);
+root.render(<h1>Willyan Carlos</h1>);
 
