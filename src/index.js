dc7600b0c05885956eda92532f9dd439fd87adc2
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-12-13 22:39:24 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <>
-    <h1>Boa noite</h1>
-    </>
+    <React.StrictMode>
+        <h1>WCFDSS</h1>
+    </React.StrictMode>
 );
 
